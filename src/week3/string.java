69df00ab2d06b9809ce8751db43647d4a66a2bff
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-01-25 21:03:21 -0500

    string

---
+++ src/week3/string.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class string {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int first = 14;
+       int second = 4;
+       
+       System.out.println(Integer.toString(first) + "+" + Integer.toString(second)+ " is " + Integer.toString(18));
+       System.out.println(Integer.toString(first) + "*" + Integer.toString(second)+ " is " + Integer.toString(56));
+       System.out.println(Integer.toString(first) + "%" + Integer.toString(second)+ " is " + Integer.toString(2));
+       System.out.println(Integer.toString(first) + "/" + Integer.toString(second)+ " is " + Integer.toString(3));
+
+    }
+    
+}
