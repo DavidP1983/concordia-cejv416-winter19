5bdf28f3202bda53e1de3e89428d36b18f1ef6c9
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-06 20:01:18 -0500

    While

---
+++ src/lecture6/class5.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture6;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class class5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        {
+            double result = 1;
+            int counter = 1;
+            
+        while (result <1000) 
+        {
+        result =  Math.pow(2, counter);
+        counter++;
+        
+        }
+        System.out.println(">>" + result);
+    }
+        
+        
+        
+    
+}
+}
+    
