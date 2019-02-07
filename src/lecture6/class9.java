be7a3623b94451aea0025db07e5df6b1adfbc182
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-06 20:38:31 -0500

    for

---
+++ src/lecture6/class9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture6;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class class9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int x=1; x<=10;x++) 
+        {
+        System.out.print(x + " ");
+        }
+        
+    }
+    
+}
