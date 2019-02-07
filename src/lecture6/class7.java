5f092de48f374a957d7481b8a3f871cb6f43f065
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-06 20:19:46 -0500

    Calculation While

---
+++ src/lecture6/class7.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture6;
+import java.util.Scanner;
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class class7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        double num1 =(int) (Math.random()*10 +1);
+         double num2 =(int) (Math.random()*10 +1); 
+         
+         
+         Scanner s = new Scanner (System.in);
+         boolean correctResponse = false;
+         
+         while (!correctResponse)
+         {
+         System.out.println("What is" +  num1 + "+" + num2 + "?");
+         double answer = s.nextDouble();
+         
+         correctResponse = (answer== num1 +num2);
+      
+         }
+         
+         System.out.println("Correct!");
+    }
+    
+    
+    
+}
