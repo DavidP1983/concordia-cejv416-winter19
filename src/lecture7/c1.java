0952d7ad658dab4ca9f4506f7f0608c11391c245
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-08 18:25:56 -0500

    for

---
+++ src/lecture7/c1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture7;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class c1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+        for (int a=2; a <= 10; a+=2){
+    System.out.print(a + " ");
+    
+    }
+       System.out.println(" ");
+       
+       for (int a=9; a >= 5; a--) {
+       System.out.print(a + " ");
+       }
+       System.out.println(" ");
+       
+       for (int a=8; a >= 0; a-=2){
+       System.out.print(a + " ");
+       
+       }
+       System.out.println(" ");
+       for (int a=1; a <= 6; a++){
+       System.out.print((int)Math.pow(2, a) + " ");
+       }
+              System.out.println(" ");
+
+    }
+    
+}
