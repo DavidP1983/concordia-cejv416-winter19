0df151e8cbb9d5f4152c56022b8852938da02550
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-08 19:19:43 -0500

    triang

---
+++ src/lecture7/c4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture7;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class c4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+//        int = 3;
+        
+        for (int y=1; y<=5; y++)
+        {
+            for (int x=1; x<=y; x++)
+                System.out.print(" * ") ;
+            
+            System.out.println(" ");
+        }
+        
+    }
+    
+}
