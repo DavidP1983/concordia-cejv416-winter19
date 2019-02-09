f6509b7abd13b44bb926760c9d5e1e0129d7b8b2
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-02-08 20:17:58 -0500

    stars2

---
+++ src/lecture7/c6.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture7;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+public class c6 {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) 
+       
+    {   
+        
+       int range = 5;
+       for (int n=1; n<=5; n++)
+       {
+       printStars(n);
+       }
+    }
+   
+     public static void printStars(int numberStars)
+     {
+     for (int x= 1; x<= numberStars; x++)
+     {
+     System.out.print("*");
+     }
+     
+     System.out.println(" ");
+     }
+    
+}
