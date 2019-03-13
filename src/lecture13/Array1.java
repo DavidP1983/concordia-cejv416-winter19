f0142929f0df3ded168f695d9af21f3322376b74
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-03-13 19:43:26 -0400

    Array1

---
+++ src/lecture13/Array1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture13;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+import java.util.Arrays;
+public class Array1 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    public static void main(String[] args) {
+
+        String[] myArray = new String[6];
+        Arrays.fill(myArray, myArray.length - 3, myArray.length, "*");
+
+        int x = 0;
+
+    }
+
+}
