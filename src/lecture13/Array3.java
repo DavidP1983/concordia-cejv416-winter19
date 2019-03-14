d6bda7c89228a1acd325d8856e42464493a2e04e
D_PIRUZA <D_PIRUZA@CCE-FB107-09W.concordia.ca>
2019-03-13 20:11:06 -0400

    ArrayList

---
+++ src/lecture13/Array3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture13;
+
+/**
+ *
+ * @author D_PIRUZA
+ */
+import java.util.ArrayList;
+public class Array3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+ArrayList<String> myList = new ArrayList<>();
+
+myList.add("Test");
+myList.add("Test2");
+        System.out.println(myList.size());
+    }
+    
+}
